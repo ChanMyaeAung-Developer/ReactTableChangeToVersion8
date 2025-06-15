5d3a116a5a0c6afdb655e7181a0898d674a5956d
Chan Myae <2000chanmyae@gamil.com>
2025-06-14 21:33:19 -0700

    Add background color depend on cell data

--- src/Tables/TableBody.jsx
+++ src/Tables/TableBody.jsx
@@ -4,24 +4,41 @@ import { flexRender } from "@tanstack/react-table";
 import ExpandableComponent from "./ExpandableComponent"; 
 
 const TableBody = ({ table, columnVisibility, dataColumns }) => {
+  const statusColorMap = {
+  "Completed": "bg-green-200 text-green-800",
+  "OSS Cancel": "bg-red-200 text-red-800",
+  "Order Cancel": "bg-yellow-200 text-red-900",
+  "Site Assignment": "bg-blue-200 text-blue-800",
+};
   return (
     <tbody className="px-4 text-content">
       {table.getRowModel().rows.map((row, rowIndex) => (
         <Fragment key={row.id}>
           <tr className="h-10 bg-[#F1F5FB] text-[12px] text-[#231F20E6] font-semibold w-full">
-            {row.getVisibleCells().map((cell) => (
-              <td
-                key={cell.id}
-                style={{ width: cell.column.getSize() || 100 }}
-                className={rowIndex % 2 ? "bg-[#F1F5FB]" : "bg-white"}
-              >
-                <span className="flex items-center justify-center gap-1.5 px-2 line-clamp-1">
-                  <p className="line-clamp-1">
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </p>
-                </span>
-              </td>
-            ))}
+          {row.getVisibleCells().map((cell) => (
+  <td
+    key={cell.id}
+    style={{ width: cell.column.getSize() || 100 }}
+    className={rowIndex % 2 ? "bg-[#F1F5FB]" : "bg-white"}
+  >
+    <span className="flex items-center justify-center gap-1.5 px-2 line-clamp-1">
+ <div
+  className={`
+    line-clamp-1 p-1 rounded text-sm font-medium
+    ${cell.column.id === "installation_status" 
+      ? statusColorMap[row.original.installation_status] || "bg-transparent text-gray-800"
+      : "bg-transparent"
+    }
+  `}
+>
+  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+</div>
+
+
+    </span>
+  </td>
+))}
+
           </tr>
 
           {row.getIsExpanded() && (
